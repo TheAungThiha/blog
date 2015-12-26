045850f6cad32bddf815f0306ce2baed6389822e
TheAungThiha <mail@aungthiha.com>
2015-12-26 12:10:19 +0800

    w esc Quit af

--- js/blog.js
+++ js/blog.js
@@ -6,10 +6,30 @@ $(function() {
     Parse.initialize("e1hTjOylez6LD007TtrLKCIfGXMNWxfSaSEJ6JSP", "dMobJdcZ9ZGKtm4FRJP1jteP1CnsKKqgGKhQRsPi");
 
  
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({blog: "nothing"}).then(function(object) {
-      alert("my first save");
-    });
+    var Blog = Parse.Object.extend("Blog");
+	var Blogs = Parse.Collection.extend({
+		model:Blog
+	});
+	
+	var blogs = new Blogs();
+	var BlogsView =  Parse.View.extend({
+    template: Handlebars.compile($('#blogs-tpl').html()),
+    render: function(){ 
+        var collection = { blog: this.collection.toJSON() };
+        this.$el.html(this.template(collection));
+		}
+	});
+	blogs.fetch({
+		success:function(blogs){
+			 var blogsView = new BlogsView({ collection: blogs });
+			blogsView.render();
+			$('.main-container').html(blogsView.el);
+		},
+		error:function(blogs,error){
+			console.log(error);
+		}
+	});
+	
+	
  
 });
